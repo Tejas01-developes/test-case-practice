26461456251310f5c1d0db48acd69f16cf5adb16
Tejas01-developes <tlimbad7@gmail.com>
2026-05-15 23:51:14 +0530

    middleware ware like acces  filter test case writting

--- src/accessfilter.ts
+++ src/accessfilter.ts
@@ -1,16 +1,24 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import jwt, {  JwtPayload } from 'jsonwebtoken';
 
-export const accessfilter=(req:JwtPayload,resp:Response,next:NextFunction)=>{
-    const token=req.header.authorization
+export interface customreq extends Request{
+id:string | number
+}
+
+export const accessfilter=(req:customreq,resp:Response,next:NextFunction)=>{
+    const token=req.headers.authorization
     const access=token?.split(" ")[1];
 
     if(!access){
         return resp.status(400).json({message:"access token is not there"})
     }
+    try{
     const decode=jwt.verify(access,process.env.ACCESS_SECRET as string) as JwtPayload
 
-    decode.id=req.id
+   req.id=decode.id
     next()
+    }catch(err){
+        return resp.status(400).json({message:"access filter failed"})
+    }
 
 }
--- src/filter.test.ts
+++ src/filter.test.ts
@@ -1,5 +1,48 @@
+import { NextFunction, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { accessfilter, customreq } from './accessfilter.js';
 
 jest.mock("jsonwebtoken");
 
-des
+describe("access filter check",()=>{
+
+    let mockreq:Partial<customreq>;
+    let mockresp:Partial<Response>;
+    let mocknext:NextFunction
+
+
+
+beforeEach(()=>{
+    mockreq={
+        headers:{}
+    },
+
+    mockresp={
+        status:jest.fn().mockReturnThis(),
+        json:jest.fn()
+    }
+    mocknext=jest.fn();
+    process.env.ACCESS_KEY="qazwsxedcrfvtgbyhnujmikolp"
+})
+
+afterEach(()=>{
+    jest.clearAllMocks()
+})
+
+
+it("should give status 400 if hear i not there",()=>{
+    mockreq.headers={authorization:"Bearer"}
+    accessfilter(mockreq as customreq,mockresp as Response,mocknext)
+    expect(mockresp.status).toHaveBeenCalledWith(400)
+    expect(mockresp.json).toHaveBeenCalledWith({message:"access token is not there"})
+    expect(mocknext).not.toHaveBeenCalled()
+
+})
+
+
+
+
+
+
+    
+})
